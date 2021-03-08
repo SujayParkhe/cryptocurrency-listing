69866c117f74f2c18a83a1162150247d8106039f
lycanzzz <62604089+lycanzzz@users.noreply.github.com>
2021-03-08 11:02:00 +0530

    Add stylings to the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import Coins from './components/Coins';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+  const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
+  
+ 
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data)
+    }).catch(err => alert('Yo there is an error'))
+  },[]);
+
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+  const filteredCoins = coins.filter(coin => 
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  )
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className='coin-app'>
+      <div className="crypto-container">
+        <header className="crypto-header">
+          <h1>Welcome to Crypto Listing</h1>
+        </header>
+        <p className="crypto-intro">
+          Crytocurrency coin information will be listed below.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency </h1>
+        <input type="text" placeholder="Search" onChange={handleChange} className="coin-input"/>
+      </div>
+      {filteredCoins.map(coin => {
+        return(
+          <Coins key={coin.id} name={coin.name} image={coin.image} symbol={coin.symbol} marketcap={coin.market_cap} price={coin.current_price} priceChange={coin.price_change_percentage_24h}/>
+        )
+      })}
     </div>
+    
   );
 }
 
